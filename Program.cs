ecba84e2352444c2b3daed76170b4f82561ea741
zachrperry <zachrperry@outlook.com>
2025-03-20 22:04:04 -0400

    funnies

--- Program.cs
+++ Program.cs
@@ -51,7 +51,21 @@ var Quotes = new[]
     "I hope you shit your pants ~Porky   Name the time and place ~Caleb",
     "I love Pico Park ~Porky",
     "I love Pico Park 2 ~Porky",
-    "Never trust anything you read on the internet ~Abraham Lincoln"
+    "Never trust anything you read on the internet ~Abraham Lincoln",
+    "Moist ~Keva",
+    "I hope you stub your toe ~Porky", 
+    "He's cheating!!! ~Karina",
+    "What is wrong with you guys ~Porky",
+    "It's so big ~Everyone who's dated v1",
+    "The G-Spot exists ~Women's Magazines",
+    "its key-vuh not kev-uh ~keva",
+    "Red flags are hot ~Candi",
+    "Stop fucking asking me to play COD ~Unknown",
+    "If you say COD you will be BANNED ~Unknown",
+    "It's me, I'm the problem ~Steph",
+    "I'm on the pill its ok ~Women",
+    "RAWR ~The Porkites",
+    "It wasn't me ~OJ/Shaggy/Porky"
 };
 
 app.MapGet("/Quotes", () =>
