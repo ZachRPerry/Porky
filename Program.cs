a9ecb8f10460c664b2a967f8906c28a2c0716a47
zachrperry <zachrperry@outlook.com>
2025-03-22 21:42:27 -0400

    LINES

--- Program.cs
+++ Program.cs
@@ -69,31 +69,57 @@ var Quotes = new[]
     "Got him right in the weiner ~Rikki",
     "I Just blew myself ~Karina",
     "I’ve been practicing like crazy and can now fit four adult farts in my mouth! ~Random",
-    "Porksicle ~Queen"
+    "Porksicle ~Queen",
+    "Hahahahaha snorts ~Rikki",
+    "Gluck Gluck MF Gluck Gluck ~Rikki"
 };
 
 app.MapGet("/Quotes", () =>
 {
     Random random = new Random();
-        string randomItem = Quotes.ElementAt(random.Next(Quotes.Length));
+        string randomItem = Lines.ElementAt(random.Next(Lines.Length));
         
     return randomItem;
 })
 .WithName("Quotes");
 
 
-var Tests = new[]
+var Lines = new[]
 {
-    "Hey @$(v1)"
+    "I was feeling off today, but you just totally turned me on",
+" If I buy you dinner, will you be dessert?",
+"If I was a judge, I’d sentence you to my bed.",
+"I’m adding you to my to-do list. Can I check you off tonight?",
+"Are you undressing me with your eyes?!",
+"Nice pants. Can I talk you out of them?",
+"It feels like it’s going to be one of those nights where neither of us get any sleep.",
+"That body is yours for the rest of your life; I just want it for tonight.",
+"Do you like bacon? Wanna strip?",
+"I’ve been super on top of things lately. Would you like to be one of them?",
+"I volunteer as your victim tonight since you're clearly dressed to kill.",
+"Do you believe in karma? Because I know some good 'Karma' Sutra positions we can try.",
+"Did you make your bed this morning? Wanna unmake it together?",
+"I find your lack of nudity disturbing.",
+"Is your car battery dead? Because I'd like to jump you right now.",
+"You remind me of Disneyland because I want to ride you all day long.",
+"Do you work at Dick’s? Because you’re sporting my favorite goods.",
+"There’s a big sale at my house right now—clothes are 100 percent off. Wanna come?",
+"What’s a nice person like you doing in a dirty mind like mine?",
+"Your body is 60 percent water…and I’m thirsty.",
+"Your bed or mine?",
+"Do you like volcanoes? Because I might erupt later.",
+"I put the std in stud and all i need is u.",
+"Do you know the difference between a boner and a cheeseburger? You're not giving me a cheeseburger right now.",
+"If I flip a coin, what are the chances of me getting head?"
 };
-app.MapGet("/Test", () =>
+app.MapGet("/Lines", () =>
 {
     Random random = new Random();
         string randomItem = Tests.ElementAt(random.Next(Tests.Length));
         
     return randomItem;
 })
-.WithName("Test");
+.WithName("Lines");
 
 app.Run();
 
