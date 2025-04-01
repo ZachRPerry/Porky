686824e664cf75c2dabf6b2c16ee8ed52a5556b8
zachrperry <zachrperry@outlook.com>
2025-04-01 17:32:14 -0400

    remove unused stuff

--- Program.cs
+++ Program.cs
@@ -14,24 +14,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var rikkis = new[]
-{
-    "It has been 0 Games Since Rikki shot Karina!",
-    "It has been 0 games since Rikki shot a teammate",
-    "RAWRRR",
-    "Have you ever had a hawaiian bowl? MMM [emote:3341441:porksliceRikki]"
-};
-
-app.MapGet("/Rikki", () =>
-{
-    Random random = new Random();
-        string randomItem = rikkis.ElementAt(random.Next(rikkis.Length));
-        
-    return randomItem;
-})
-.WithName("Rikki");
-
-
 
 var Quotes = new[]
 {
@@ -74,7 +56,6 @@ var Quotes = new[]
     "Gluck Gluck MF Gluck Gluck ~Rikki",
     "I am going to try and go live in the morning ~Porky before not going live in the morning",
     "Fuck me sideways ~Tenisha",
-    "I'll give you a free SND game for finding this hidden quote ~Porky (this is real and not fake)", 
     "What did you throw at him? Your number? ~Porky",
     "Did he just call me a whore rag? ~Porky",
     "I GOT ELECTROCUTIONED! ~Ash",
@@ -93,42 +74,6 @@ app.MapGet("/Quotes", () =>
 .WithName("Quotes");
 
 
-var Lines = new[]
-{
-    "I was feeling off today, but you just totally turned me on",
-" If I buy you dinner, will you be dessert?",
-"If I was a judge, I’d sentence you to my bed.",
-"I’m adding you to my to-do list. Can I check you off tonight?",
-"Are you undressing me with your eyes?!",
-"Nice pants. Can I talk you out of them?",
-"It feels like it’s going to be one of those nights where neither of us get any sleep.",
-"That body is yours for the rest of your life; I just want it for tonight.",
-"Do you like bacon? Wanna strip?",
-"I’ve been super on top of things lately. Would you like to be one of them?",
-"I volunteer as your victim tonight since you're clearly dressed to kill.",
-"Do you believe in karma? Because I know some good 'Karma' Sutra positions we can try.",
-"Did you make your bed this morning? Wanna unmake it together?",
-"I find your lack of nudity disturbing.",
-"Is your car battery dead? Because I'd like to jump you right now.",
-"You remind me of Disneyland because I want to ride you all day long.",
-"Do you work at Dick’s? Because you’re sporting my favorite goods.",
-"There’s a big sale at my house right now—clothes are 100 percent off. Wanna come?",
-"What’s a nice person like you doing in a dirty mind like mine?",
-"Your body is 60 percent water…and I’m thirsty.",
-"Your bed or mine?",
-"Do you like volcanoes? Because I might erupt later.",
-"I put the std in stud and all i need is u.",
-"Do you know the difference between a boner and a cheeseburger? You're not giving me a cheeseburger right now.",
-"If I flip a coin, what are the chances of me getting head?"
-};
-app.MapGet("/Lines", () =>
-{
-    Random random = new Random();
-        string randomItem = Lines.ElementAt(random.Next(Lines.Length));
-        
-    return randomItem;
-})
-.WithName("Lines");
 
 app.Run();
 
